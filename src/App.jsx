52eeea93104bf551e51ebde2f4ba29e8c64ee519
Sam Nockels <samnockels@gmail.com>
2020-03-11 09:54:50 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { version } from "antd";
+
+class App extends React.Component {
+  render(){
+    return 'hi'
+  }
+}
+
+export default App
